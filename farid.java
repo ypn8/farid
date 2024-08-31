56bc42edae39624d3ef192b8a5c9e38a8b92aa76
SHAIK FARID <shaikfarid524@gmail.com>
2024-08-31 17:36:51 +0530

    first commit

---
+++ farid.java
@@ -0,0 +1,13 @@
+class ex
+{
+	public static void main(String args[])
+	{
+		String S1="FARID";
+	char ch[]={'j','a','b','i','r',};
+	String S2=new String(ch);
+	String S3=new String("NARASIMHA SIR");
+	System.out.println(S1);
+	System.out.println(S2);
+	System.out.println(S3);
+	}
+}
